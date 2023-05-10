f01da127064fc2cff4eecd9d22f10c0b08123c1d
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-05-10 14:24:33 +0800

    Ask the user to input the first and second number

--- calculator.py
+++ calculator.py
@@ -8,4 +8,9 @@
     # If yes, repeat Step 1.
     # If no, Display “Thank you!” and the program will exit
     # Use Python Function and appropriate Exceptions to capture errors during runtime.
-
+    
+def calculator():
+    
+# Ask the user to input the first and second number
+    num1 = float(input("Please enter your first number: "))
+    num2 = float(input("Please enter your second number: "))
